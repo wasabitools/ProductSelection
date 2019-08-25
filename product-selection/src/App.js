d4705c4d0f7525370780428490b252216c905b54
natasafi <natasafrag@gmail.com>
2019-08-25 20:35:26 +0100

    introduces the list of all the show categories

--- product-selection/src/App.js
+++ product-selection/src/App.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import Header from './components/Header/Header'
-import Sports from './components/Sports'
-import News from './components/News'
+import Sports from './components/Products/Products'
 import './App.css';
 
 function App() {
   return (
     <div className='App'>
       <Header/>
-      {/* <Sports />
-      <News/> */}
+      <Sports />
     </div>
      );
 }
---
+++ product-selection/src/components/Products/Products.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import "./Products.css"
+import Shows from '/home/natasafi/Desktop/ProductSelection/product-selection/src/data.json'
+
+const data = JSON.stringify(Shows)
+const parsed = JSON.parse(data)
+
+class Products extends Component {
+  state = {
+    shows: parsed
+
+  }
+  render() {
+  
+    return (
+      <main>
+
+        <div className='sports'>
+          <h3>Sports TV</h3>
+          <ul>
+            {this.state.shows.map(show => {
+              if (show.type === 'sports') {
+                return <li key={show.id}>{show.title}</li>
+              }
+            })}
+          </ul>
+        </div>
+        <div className='news'>
+          <h3>News TV</h3>
+          <ul>
+            {this.state.shows.map(show => {
+              if (show.type === 'news') {
+                return <li key={show.id}>{show.title}</li>
+              }
+            })}
+          </ul>
+        </div>
+      </main>
+    );
+  }
+  }
+
+
+export default Products;
