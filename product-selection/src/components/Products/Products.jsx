0e367b9c2c301e07f715f7adb8fbd23d92a6952d
natasafi <natasafrag@gmail.com>
2019-08-25 22:14:01 +0100

    introduces checkbox property with style

--- product-selection/src/components/Products/Products.jsx
+++ product-selection/src/components/Products/Products.jsx
@@ -1,25 +1,30 @@
 import React, { Component } from 'react';
-import "./Products.css"
-import Shows from '/home/natasafi/Desktop/ProductSelection/product-selection/src/data.json'
+import "./Products.css";
+import Shows from '/home/natasafi/Desktop/ProductSelection/product-selection/src/data.json';
+import Checkbox from '../Checkbox';
 
 const data = JSON.stringify(Shows)
 const parsed = JSON.parse(data)
 
 class Products extends Component {
   state = {
-    shows: parsed
+    shows: parsed,
+    checked:false
   }
   render() {
-  
+    const data = this.state.shows
     return (
-      <main>
-
+      <main>       
         <div id='sports'>
           <h3>Sports TV</h3>
           <ul>
-            {this.state.shows.map(show => {
+            {data.map(show => {
+              
               if (show.type === 'sports') {
-                return <li key={show.id}>{show.title}</li>
+                return (
+                  <li key={show.id}>
+                    <Checkbox className="check" handleCheckChildElement={this.handleCheckChildElement} />{show.title}</li>
+                )
               }
             })}
           </ul>
@@ -29,7 +34,10 @@ class Products extends Component {
           <ul>
             {this.state.shows.map(show => {
               if (show.type === 'news') {
-                return <li key={show.id}>{show.title}</li>
+                return (<li key={show.id}>
+                  <Checkbox className="check" handleCheckChildElement={this.handleCheckChildElement} />
+                  {show.title}
+                </li>)
               }
             })}
           </ul>
@@ -37,6 +45,15 @@ class Products extends Component {
       </main>
     );
   }
+
+  // handleCheckChildElement = (event) => {
+  //   let shows = this.state.shows
+  //   shows.forEach(show => {
+  //     if (show.value === event.target.value)
+  //       show.isChecked = event.target.checked
+  //   })
+  //   this.setState({ shows: shows })
+  // }
   }
 
 
